e8ac68425943bd777da566495b1162a43cb7d896
kuldeepsinghchouha <kuldeepsinghchouhan1997@gmail.com>
2023-05-25 03:40:54 +0530

    Commit 2

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "testdeploymenttask": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
